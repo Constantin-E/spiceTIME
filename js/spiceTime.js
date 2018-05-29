7df18329ab5c59d80f1619882d678a5bec76fb69
Constantin Ehrensberger <ehrensberger.c@gmail.com>
2018-05-29 17:51:46 +0300

    setup to show other list

--- js/spiceTime.js
+++ js/spiceTime.js
@@ -2,7 +2,15 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            backgroundListTitle: "Completed Tasks",
+            shownListTitle: "To Do"
         };
+        this.sho...
+    }
+    showOtherList(){
+        //change title pops and rerender List component
+        //change innerHTML of background-list
+        //---> switch backgroundlisttitle and shownlisttitle in state
     }
     render() {
         return (
@@ -10,8 +18,10 @@ class App extends React.Component {
                 <div className="header">
                 SpiceTime
                 </div>
-                <div className="background-list">Completed</div>
-                <div className="shown-list"></div>
+                <div className="background-list" onClick={this.showOtherList}>{this.state.backgroundListTitle}</div>
+                <div className="shown-list">
+                    <List title={this.state.shownListTitle}/>
+                </div>
             </div>
         );
     }
