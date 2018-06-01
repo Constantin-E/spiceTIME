44d537adc93ea9be921e45d1f72011498e739cb9
Constantin Ehrensberger <ehrensberger.c@gmail.com>
2018-06-01 19:19:13 +0300

    deleted buggy additem code

--- js/oldSpice.js
+++ js/oldSpice.js
@@ -5,7 +5,6 @@ class App extends React.Component {
             backgroundListTitle: "Completed Tasks",
             shownListTitle: "To Do",
             newItemFieldShown: false,
-            newItemText: null,
         };
         this.showOtherList = this.showOtherList.bind(this);
         this.showNewItemField = this.showNewItemField.bind(this);
@@ -23,9 +22,8 @@ class App extends React.Component {
             newItemFieldShown: !this.state.newItemFieldShown
         });
     }
-    addNewItem(newItemText) {
+    addNewItem() {
         this.setState({
-            newItemText: newItemText,
             newItemFieldShown: false
         })
     }
@@ -43,7 +41,7 @@ class App extends React.Component {
                         <span className="add-chili-btn" onClick={this.showNewItemField}><i class="fas fa-plus"></i></span>
                     </h2>
                     <NewItemField isShown={this.state.newItemFieldShown} newItem={this.addNewItem} />
-                    <List title={this.state.shownListTitle} newItemText={this.state.newItemText} />
+                    <List title={this.state.shownListTitle} />
                 </div>
             </div>
         );
@@ -53,23 +51,17 @@ class NewItemField extends React.Component {
     constructor(props) {
         super(props);
         this.showClass = this.showClass.bind(this);
-        this.addItem = this.addItem.bind(this);
     }
 
     showClass() {
         return this.props.isShown ? null : "not-displayed";
     }
-    addItem() {
-        let newItemText = this.newItemTextField.value;
-        this.props.newItem(newItemText);
-        this.newItemTextField.value = null;
-    }
 
     render() {
         return (
             <div className={"list-item-container " + this.showClass()}>
-                <input type="text" placeholder="Type new task here" className="add-task-input" ref={x => (this.newItemTextField = x)} />
-                <button class="add-btn" onClick={this.addItem}>Add!</button>
+                <input type="text" placeholder="Type new task here" className="add-task-input" />
+                <button class="add-btn" >Add!</button>
             </div>
         )
     }
@@ -87,22 +79,13 @@ class List extends React.Component {
                 ["empty", "", false],
                 ["empty", "", true], //at least one of these should always be appended to each list in the end. if space beneath is empty (e.g. this.state.listData[itemInted][2]== true for numOfListItems element, and numOfListItems<10, fill up List with 10-numOfListItems empty items. )
             ],
-            listIsUpdated: true
         }
         this.showListItems = this.showListItems.bind(this);
-        this.importNewItem = this.importNewItem.bind(this);
         this.markAsDone = this.markAsDone.bind(this);
     }
     // make delete items button  -- this will take uniquenumber and simply do : delete listData[itemindex] 
     //the right item index is the one with listData[itemIndex][0] = uniqueNumber
-    importNewItem() {
-        let currentListData = this.state.listData;
-        let lastItemId = currentListData[0][0];
-        let newItemArr = [lastItemId + 1, this.props.newItemText, false, false, null];
-        currentListData.unshift(newItemArr)
-        this.state.listData = currentListData
-
-    }
+    
     markAsDone(i) {
         // let selected = this.selectItem(e);
         // let selectedItemIndex = i;
@@ -116,8 +99,6 @@ class List extends React.Component {
         });
     }
     showListItems() {
-        console.log("list received new item: " + this.props.newItemText);
-        (this.props.newItemText && this.state.listIsUpdated) ? this.importNewItem() : null;
         let showCompletedTasks = (this.props.title === "To Do") ? false : true;
         let itemArr = [];
         for (var itemIndex = 0; itemIndex < this.state.listData.length; itemIndex++) {
