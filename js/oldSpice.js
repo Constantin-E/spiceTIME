1b92868d3d226b91bc32774369020543ea3e2918
Constantin Ehrensberger <ehrensberger.c@gmail.com>
2018-06-01 23:34:27 +0300

    buttons are now hover

--- js/oldSpice.js
+++ js/oldSpice.js
@@ -87,11 +87,9 @@ class List extends React.Component {
             listData: [
                 // [uniqueNumber, text, isCompleted, isChili, reminder]
                 [3, "My First Task", false, false, null],
-                [2, "My Second Task", true, false, null],
-                [1, "My Third Task", false, true, null],
-                [0, "My Fourth Task", true, true, null],
-                ["empty", "", false],
-                ["empty", "", true], //at least one of these should always be appended to each list in the end. if space beneath is empty (e.g. this.state.listData[itemInted][2]== true for numOfListItems element, and numOfListItems<10, fill up List with 10-numOfListItems empty items. )
+                [2, "My First Done Task", true, false, null],
+                [1, "My Second Task", false, true, null],
+                [0, "My Second Done Task", true, true, null],
             ],
         }
         this.showListItems = this.showListItems.bind(this);
@@ -104,7 +102,7 @@ class List extends React.Component {
 
     componentWillReceiveProps(nextProps) {
         console.log("check to update listadata fires");
-        if (nextProps.newItemText != this.state.listData[0][1] && nextProps.newItemText !=null) {
+        if (nextProps.newItemText != this.state.listData[0][1] && nextProps.newItemText != null) {
             let currentListData = this.state.listData;
             let lastItemId = currentListData[0][0];
             let newItemArr = [lastItemId + 1, nextProps.newItemText, false, false, null];
@@ -152,20 +150,11 @@ class List extends React.Component {
                     <div className="list-item-container">
                         <button onClick={this.markAsDone} listIndex={itemIndex}><i class="fa fa-check"></i></button>
                         <span>{this.state.listData[itemIndex][1]}</span>
-                        {/* <div className="mock-buttons-container">
-                            <div className="add-spice-button"></div>
-                            <div className="delete-task-button" id={"buttonFor" + this.state.listData[itemIndex][0]}></div>
-                        </div> */}
-                        <span>
+                        <div className="mock-buttons-container">
                             <button onClick={this.deleteItem} listIndex={itemIndex}><i class="fa fa-trash"></i></button>
-                        </span>
-                        <button onClick={this.markAsChili} listIndex={itemIndex}>Spice it up!</button>
-                        {/* chili button */}
-                        <span>
+                            <button onClick={this.markAsChili} listIndex={itemIndex}>Spice it up!</button>
                             <button><i class="fa fa-bell"></i></button>
-                        </span>
-                        {/* hoverButtons */}
-
+                        </div>
                     </div>
                 )
                 itemArr.push(item);
